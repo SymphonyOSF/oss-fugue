46f52169254229fd47e1ca8db5022844a20e1e98
GeremiaLongobardo <geremia.longobardo@symphony.com>
2020-08-26 16:45:41 +0200

    reverted to amazon original

--- fugue-aws/src/main/java/com/symphony/oss/fugue/aws/sqs/GatewayAmazonSQSClientBuilder.java
+++ fugue-aws/src/main/java/com/symphony/oss/fugue/aws/sqs/GatewayAmazonSQSClientBuilder.java
@@ -1,34 +1,32 @@
 /*
- *
- *
- * Copyright 2020 Symphony Communication Services, LLC.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2015-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with
+ * the License. A copy of the License is located at
+ * 
+ * http://aws.amazon.com/apache2.0
+ * 
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
  */
-
 package com.symphony.oss.fugue.aws.sqs;
 
+import javax.annotation.Generated;
+
+import com.amazonaws.annotation.NotThreadSafe;
 import com.amazonaws.client.AwsSyncClientParams;
 import com.amazonaws.client.builder.AwsSyncClientBuilder;
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 
 /**
- * SQSClientBuilder to redirect the calls to the Api Gateway
- * 
- * @author Geremia Longobardo
- *
- */
+ * Fluent builder for {@link com.amazonaws.services.sqs.AmazonSQS}. Use of the builder is preferred over using
+ * constructors of the client class.
+ * ** CLASS MODIFIED IN ORDER TO BUILD THE MODIFIED SQSCLIENT **
+ **/
+@NotThreadSafe
+@Generated("com.amazonaws:aws-java-sdk-code-generator")
 public class GatewayAmazonSQSClientBuilder extends AwsSyncClientBuilder<AmazonSQSClientBuilder, AmazonSQS> {
 
   //private static final ClientConfigurationFactory CLIENT_CONFIG_FACTORY = new com.amazonaws.services.sqs.AmazonSQSClientConfigurationFactory();
